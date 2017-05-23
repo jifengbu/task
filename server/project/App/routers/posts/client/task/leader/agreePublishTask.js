9591cbff396aada5733ec6778f6e1c3b128fe3d8
fov42550564 <42550564@qq.com>
2017-05-23 09:19:25 +0800

    修正任务状态

--- server/project/App/routers/posts/client/task/leader/agreePublishTask.js
+++ server/project/App/routers/posts/client/task/leader/agreePublishTask.js
@@ -1,8 +1,17 @@
 import { TaskGroupModel, TaskModel } from '../../../../../models';
 
 export default async ({ userId, taskId }) => {
-    const doc = await TaskModel.findByIdAndUpdate(taskId, {state: 2});
-    await TaskGroupModel.findByIdAndUpdate(doc.groupId, {state: 2});
-
+    const taskGroup = await TaskGroupModel.findById(doc.groupId)
+    .select({
+        taskList: 1,
+    }).populate({
+        path: 'taskList',
+        select: {
+            state:1,
+        },
+    });
+    taskGroup.state = _.reduce(taskGroup.taskList.map(o=>o.state), (r, o)=>r|o);
+    await taskGroup.save();
+    
     return { success: true };
 };
--- server/project/App/routers/posts/client/task/leader/rejectPublishTask.js
+++ server/project/App/routers/posts/client/task/leader/rejectPublishTask.js
@@ -1,9 +1,19 @@
 import { TaskGroupModel, TaskModel } from '../../../../../models';
+import _ from 'lodash';
 
 export default async ({ userId, taskId }) => {
     const doc = await TaskModel.findByIdAndUpdate(taskId, {state: 4});
-    console.log("=========", doc);
-    await TaskGroupModel.findByIdAndUpdate(doc.groupId, {state: 4});
-
+    const taskGroup = await TaskGroupModel.findById(doc.groupId)
+    .select({
+        taskList: 1,
+    }).populate({
+        path: 'taskList',
+        select: {
+            state:1,
+        },
+    });
+    taskGroup.state = _.reduce(taskGroup.taskList.map(o=>o.state), (r, o)=>r|o);
+    await taskGroup.save();
+    
     return { success: true };
 };
